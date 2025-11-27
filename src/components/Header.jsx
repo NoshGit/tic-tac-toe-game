4b7286a727c5f56e55a0e653b7ceab2a90676b2e
Noshir Patel <noshirpatel86@gmail.com>
2025-11-27 22:56:53 +0530

    added title as dynamic prop with default value

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-export default function Header() {
+export default function Header({ title = 'Tic-Tac-Toe' }) {
   return (
     <header>
       <img
         src="./../../public/game-logo.png"
         alt="Hand Drawn Tic Tac Toe logo"
       />
-      <h1>Tic Tac Toe game</h1>
+      <h1>{title}</h1>
     </header>
   );
 }
