55f02eeb0f4472f3d38fd3ef593e9a3227c280e5
Noshir Patel <noshirpatel86@gmail.com>
2025-11-27 22:55:32 +0530

    Made player names dynamic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
+  const [editMode, setEditMode] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  const handleEditClick = () => {
+    setEditMode((editMode) => !editMode);
+  };
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editMode ? (
+          <input
+            type="text"
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{editMode ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
