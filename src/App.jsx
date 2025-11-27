12839adcab6b8c000604eca3a54bab8739b9b4e2
Noshir Patel <noshirpatel86@gmail.com>
2025-11-27 19:11:49 +0530

    Created Basic Game Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <>
+      <header>
+        <img
+          src="./../public/game-logo.png"
+          alt="Hand Drawn Tic Tac Toe logo"
+        />
+        <h1>Tic Tac Toe game</h1>
+      </header>
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <li>
+              <span className="player">
+                <span className="player-name">Player 1</span>
+                <span className="player-symbol">X</span>
+              </span>
+              <button>Edit</button>
+            </li>
+            <li>
+              <span className="player">
+                <span className="player-name">Player 2</span>
+                <span className="player-symbol">O</span>
+              </span>
+              <button>Edit</button>
+            </li>
+          </ol>
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
